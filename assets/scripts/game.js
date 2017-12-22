abec1202fe42ef3f5d6bb9c3c71b9da0362663a7
nandodonaire <nandodonaire@gmail.com>
2017-12-22 09:44:51 -0500

    Add nextTurn variable

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -11,6 +11,8 @@ const currentGame = {
   player_o: null
 }
 
+const nextTurn = 'x'
+
 const checkForWinner = function () {
   if ((currentGame.cells[0] === 'x' && currentGame.cells[1] === 'x' && currentGame.cells[2] === 'x') || (currentGame.cells[3] === 'x' && currentGame.cells[4] === 'x' && currentGame.cells[5] === 'x') || (currentGame.cells[6] === 'x' && currentGame.cells[7] === 'x' && currentGame.cells[8] === 'x') || (currentGame.cells[0] === 'x' && currentGame.cells[3] === 'x' && currentGame.cells[6] === 'x') || (currentGame.cells[1] === 'x' && currentGame.cells[4] === 'x' && currentGame.cells[7] === 'x') || (currentGame.cells[0] === 'x' && currentGame.cells[1] === 'x' && currentGame.cells[2] === 'x') || (currentGame.cells[0] === 'x' && currentGame.cells[4] === 'x' && currentGame.cells[8] === 'x') || (currentGame.cells[2] === 'x' && currentGame.cells[4] === 'x' && currentGame.cells[6] === 'x')) {
     console.log('x is the winner!')
@@ -27,11 +29,11 @@ $('#index-zero').on('click', function () {
 
   // const i = 0
   // if (currentGame.cells[i] === '') {
-  $('#index-zero').text('x')
+  $('#index-zero').text(nextTurn)
   // } else {
   //   console.log("You can't add to this space")
   // }
-  currentGame.cells.splice(0, 1, 'x')
+  currentGame.cells.splice(0, 1, nextTurn)
   console.log(currentGame.cells)
   checkForWinner()
 })
