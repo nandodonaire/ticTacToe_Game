ca1339b0ce9783f4b10ce46c58c6d6f1bbe11271
nandodonaire <nandodonaire@gmail.com>
2017-12-26 12:27:03 -0500

    Add game API functionality

---
+++ scripts/json/create.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# sh scripts/json/create.sh
+curl "https://aqueous-atoll-85096.herokuapp.com/games" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{}'
---
+++ scripts/json/index.sh
@@ -0,0 +1,6 @@
+curl "https://aqueous-atoll-85096.herokuapp.com/games/" \
+  --include \
+  --request GET \
+  --header "Authorization: Token token=${TOKEN}"
+
+echo
